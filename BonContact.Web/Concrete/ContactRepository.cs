410414dedc7f3dcf4edfedc363668624c6ae356e
knolegsb <seon2you@hotmail.com>
2016-07-12 01:17:28 -0700

    changed route

---
+++ BonContact.Web/Concrete/ContactRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BonContact.Web.Concrete
+{
+    public class ContactRepository
+    {
+    }
+}
